6222f077c5f064dbf7837b58ae6b67b9551c98d6
al002 <al002@disroot.org>
2025-03-02 00:08:52 +0800

    udp tracker

--- internal/tracker/udp.go
+++ internal/tracker/udp.go
@@ -10,33 +10,33 @@ import (
 )
 
 type UdpClient struct {
-  cl *udpTracker.Client
+	cl *udpTracker.Client
 }
 
 func (c *UdpClient) Close() error {
-  return c.cl.Close()
+	return c.cl.Close()
 }
 
 func (c *UdpClient) Announce(
-  ctx context.Context,
-  req trackerTypes.AnnounceRequest,
-  opts trHttp.AnnounceOpt,
+	ctx context.Context,
+	req trackerTypes.AnnounceRequest,
+	opts trHttp.AnnounceOpt,
 ) (res trHttp.AnnounceResponse, err error) {
-  if req.IPAddress == 0 && opts.ClientIpV4 != nil{
-    req.IPAddress = binary.BigEndian.Uint32(opts.ClientIpV4.To4())
-  }
+	if req.IPAddress == 0 && opts.ClientIpV4 != nil {
+		req.IPAddress = binary.BigEndian.Uint32(opts.ClientIpV4.To4())
+	}
 
-  h, nas, err := c.cl.Announce(ctx, req)
-  if err != nil {
-    return
-  }
+	h, nas, err := c.cl.Announce(ctx, req)
+	if err != nil {
+		return
+	}
 
-  res.Interval = h.Interval
-  res.Leechers = h.Leechers
-  res.Seeders = h.Seeders
-  for _, cp := range nas.NodeAddrs() {
-    res.Peers = append(res.Peers, trHttp.Peer{}.FromNodeAddr(cp))
-  }
+	res.Interval = h.Interval
+	res.Leechers = h.Leechers
+	res.Seeders = h.Seeders
+	for _, cp := range nas.NodeAddrs() {
+		res.Peers = append(res.Peers, trHttp.Peer{}.FromNodeAddr(cp))
+	}
 
-  return
+	return
 }
